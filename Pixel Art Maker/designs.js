a4e70c82cdcb16b1c5d0663628aad23a9cac5d7e
yawmiej <yomlateef@yahoo.com>
2018-12-07 01:19:35 +0100

    Created Table grid functionality

--- Pixel Art Maker/designs.js
+++ Pixel Art Maker/designs.js
@@ -1,20 +1,33 @@
-function makeGrid(r,c){
-    const table = document.querySelector('#pixelCanvas');
-}
-function appe(){
-    const table = document.querySelector('#pixelCanvas');
-    let trow = document.createElement('tr');
-    trow.setAttribute('id', 'table-row');
-    let tcol = document.createElement('td');
-    table.append(trow);
-    document.querySelector('#table-row').append(tcol);
+const table = document.querySelector('#pixelCanvas');
 
+function makeGrid(r = 1,c = 1){
+    let oldRow = document.querySelectorAll('tr');
+    for (let n = 0; n < oldRow.length; n++ ) {
+        oldRow[n].remove();
+    }
+    for (let i = 1; i <= r; i++) {
+        
+        let trow = document.createElement('tr');
+        trow.setAttribute('id', `table-row${i}`);
+        table.append(trow);
+        for (let j = 1; j <= c; j++) {
+            let tcol = document.createElement('td');
+            document.querySelector(`#table-row${i}`).append(tcol);
+        }
+    }
 }
 
 const submit = document.querySelector("#submit");
 submit.addEventListener('click', function(event){
-    const row = document.querySelector('#inputWidth').value;
-    const column = document.querySelector('#inputHeight').value;
+    let row = document.querySelector('#inputWidth').value;
+    let column = document.querySelector('#inputHeight').value;
     makeGrid(row, column);
+    row = 1;
+    column = 1;
     event.preventDefault();
 });
+
+
+table.addEventListener('click', function(){
+
+})
